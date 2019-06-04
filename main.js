b9a2d566cf91110f4de74c0e3420f0fcaf66fdb6
Kristin Lam <10857587+kristinlam@users.noreply.github.com>
2019-06-04 03:14:22 -0400

    wip website

---
+++ main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+	var $grid = $('.grid').isotope({
+		// options...
+		itemSelector: '.grid-item',
+		// sortBy: 'random'
+	});
+
+	// layout Isotope after each image loads
+	$grid.imagesLoaded().progress( function() {
+		$grid.isotope('layout');
+
+	// filter items on button click
+		$('.filter-button-group').on( 'click', 'li', function() {
+		  var filterValue = $(this).attr('data-filter');
+			  $('.grid').isotope({ filter: filterValue });
+			  $('.filter-button-group li').removeClass('active');
+			  $(this).addClass('active');
+		});
+
+	});
+});
+
+
